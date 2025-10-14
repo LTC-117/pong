1429133f9ec6385187190e06e8186ca8a1d5b036
LTC-117 <lucastentoni@gmail.com>
2025-10-14 20:14:30 -0300

    [Restructuring main function]

--- src/main.c
+++ src/main.c
@@ -70,32 +70,6 @@ int main(void)
                     break;
             }
 
-//            uint32_t current_time = SDL_GetTicks();
-//            double delta_time = (current_time - last_time) / 1000.0;  // Time (in seconds)
-//            last_time = current_time;
-//
-//            //Racket movement based on time
-//            if(SDL_GetKeyboardState(NULL)[SDL_SCANCODE_J]) {
-//                racket.racket_pos += racket.racket_speed * delta_time;  //Moves down
-//            }
-//            if(SDL_GetKeyboardState(NULL)[SDL_SCANCODE_K]) {
-//                racket.racket_pos -= racket.racket_speed * delta_time;  //Moves up
-//            }
-//
-//            //Ensures the racket stays within the screen boundaries
-//            if(racket.racket_pos < 0) racket.racket_pos = 0;
-//            if(racket.racket_pos + 100 > SCREEN_HEIGHT) racket.racket_pos = SCREEN_HEIGHT - 100;
-//
-//            ball_trajectory_fill(surface, trajectory, trajectory_entry_count);
-//            ball_fill(surface, ball, WHITE);
-//            table_create(surface);
-//            racket_create(surface, &racket);
-//
-//            ball_physics(&ball, &racket);
-//            ball_trajectory_update(trajectory, ball, trajectory_entry_count);
-//
-//            if(trajectory_entry_count < TRAJECTORY_LENGTH) ++trajectory_entry_count;
-
             //Updates the window
             SDL_UpdateWindowSurface(window);
             SDL_Delay(1);  //Adjusts the delay if needed to control the FPS
